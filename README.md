49e1f6942d1345a836f784ef593ef86eabab0a0c
smeske10 <35307525+smeske10@users.noreply.github.com>
2022-03-22 13:19:56 -0400

    Added sections for frontend app

--- README.md
+++
@@ -1,2 +0,0 @@
-# React Coding Portfolio
- This web page, built with ReactJS, showcases all my development projects
---
+++ reactjscodingportfolio/frontend_react/src/container/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Footer.scss'
+
+export const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
