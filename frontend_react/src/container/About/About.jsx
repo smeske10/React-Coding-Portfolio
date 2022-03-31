dc3f4525a9356b5aadcda8e57cc6fa4eb6a47db0
smeske10 <35307525+smeske10@users.noreply.github.com>
2022-03-31 13:31:17 -0400

    Updated File Structure and Added About Section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
+// import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
 import {urlFor, client} from '../../client'
 
-export const About = () => {
+const About = () => {
 
-  const [aboutArray, setAbouts] = useState([]);
+  const [abouts, setAbouts] = useState([]);
 
   useEffect(() => {
-    const query = '*[_type == "aboutArray"]';
+    const query = '*[_type == "abouts"]';
 
     client.fetch(query).then((data) => setAbouts(data))
   }, [])
@@ -21,21 +22,27 @@ export const About = () => {
       </h2>
 
       <div className='app__profiles'>
-        {aboutArray.map((about, index) => (
+        {abouts.map((about, index) => (
           <motion.div
           whileInView={{opacity: 1}}
           whileHover={{scale: 1.1}}
           transition={{duration: 0.5, type: 'tween'}}
           className= "app__profile-item"
           key={about.title + index}>
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
             <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
           </motion.div>
         ))}
       </div>
     </>
-  )
-}
+  );
+};
+
+// export default AppWrap(
+//   MotionWrap(About, 'app__about'),
+//   'about',
+//   'app__whitebg',
+// );
 
 export default About;
