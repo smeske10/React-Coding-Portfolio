4fcb93dea31f45fed31941f6a75b336597b60018
smeske10 <35307525+smeske10@users.noreply.github.com>
2022-04-23 21:16:03 -0400

    Updated Navbar and Social Media Links

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -3,16 +3,20 @@ import React from "react";
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-      {["home", "about", "work", "skills", "testimonials", "contact"].map(
-        (item, index) => (
-          <a
-            href={`#${item}`}
-            key={item + index}
-            className="app__navigation-dot"
-            style={active === item ? { backgroundColor: "#313bac" } : {}}
-          />
-        )
-      )}
+      {[
+        "home",
+        "about",
+        "work",
+        // "skills", "testimonials",
+        "contact",
+      ].map((item, index) => (
+        <a
+          href={`#${item}`}
+          key={item + index}
+          className="app__navigation-dot"
+          style={active === item ? { backgroundColor: "#313bac" } : {}}
+        />
+      ))}
     </div>
   );
 };
--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import { BsTwitter, BsInstagram} from 'react-icons/bs';
-import { FaFacebook } from 'react-icons/fa';
+import React from "react";
+import { BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs";
 
 const SocialMedia = () => {
   return (
-    <div className='app__social'>
-        <div>
-            <BsTwitter />
-        </div>
-        <div>
-            <FaFacebook />
-        </div>
-        <div>
-            <BsInstagram />
-        </div>
+    <div className="app__social">
+      <div>
+        <a href="https://twitter.com/smeske10">
+          <BsTwitter />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/sam-meske-ms-mba/">
+          <BsLinkedin />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.instagram.com/sammeske/">
+          <BsInstagram />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SocialMedia;
